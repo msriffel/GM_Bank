4c7ddaff18a7ce224a9f2df9584379a61085bad2
msriffel <mr0109852@setrem.com.br>
2023-11-15 17:52:05 -0300

    Toast e correçoes

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -21,15 +21,58 @@ export class CadastroComponent {
     private correntistaService: CorrentistaService,
     private router: Router,
     private message: MessageService
-    ) {
+  ) {
     this.obterProximoNumeroConta();
   }
 
+  home() {
+    this.router.navigate(['principal']);
+  }
+
   cadastrar() {
-    if (!this.validarCampos()) {
-      this.message.add({
-        severity: 'warn', summary: 'Aviso!', detail: 'Por favor, preencha todos os campos.'
-      });
+    // Função para validar CPF
+    const validarCPF = (): boolean => {
+      console.log('Validando CPF...');
+      const cpf = this.cpf.replace(/\D/g, ''); // Remover caracteres não numéricos
+      console.log('CPF digitado:', cpf);
+      if (cpf.length !== 11) {
+        this.message.add({
+          severity: 'warn', summary: 'Aviso!', detail: 'CPF deve ter 11 dígitos.'
+        });
+        return false;
+      }
+
+      // Algoritmo de validação do CPF
+      let sum = 0;
+      for (let i = 0; i < 9; i++) {
+        sum += parseInt(cpf.charAt(i)) * (10 - i);
+      }
+
+      const remainder = (sum * 10) % 11;
+      if (remainder !== parseInt(cpf.charAt(9))) {
+        this.message.add({
+          severity: 'warn', summary: 'Aviso!', detail: 'CPF inválido.'
+        });
+        return false;
+      }
+
+      sum = 0;
+      for (let i = 0; i < 10; i++) {
+        sum += parseInt(cpf.charAt(i)) * (11 - i);
+      }
+
+      const secondRemainder = (sum * 10) % 11;
+      if (secondRemainder !== parseInt(cpf.charAt(10))) {
+        this.message.add({
+          severity: 'warn', summary: 'Aviso!', detail: 'CPF inválido.'
+        });
+        return false;
+      }
+
+      return true;
+    };
+
+    if (!this.validarCampos() || !validarCPF()) {
       return;
     }
 
@@ -53,14 +96,17 @@ export class CadastroComponent {
       },
       (error) => {
         this.message.add({
-        severity: 'error', summary: 'Erro!', detail: error
-      });
+          severity: 'error', summary: 'Erro!', detail: 'Contate o administrador do sistema!'
+        });
       }
     );
   }
 
   private validarCampos(): boolean {
     if (!this.nome || !this.dataNascimento || !this.chavePix || !this.cpf) {
+      this.message.add({
+        severity: 'warn', summary: 'Aviso!', detail: 'Por favor, preencha todos os campos.'
+      });
       return false;
     }
 
